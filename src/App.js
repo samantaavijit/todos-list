b451e71b6116f71a785f089384ac04579eac5b0d
Avijit Samanta <67010969+samantaavijit@users.noreply.github.com>
2022-05-02 16:43:09 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './MyComponents/Header'
+import { Footer } from './MyComponents/Footer'
+import { Todos } from './MyComponents/Todos'
+import {AddTodo} from './MyComponents/AddTodo'
+import React, { useState } from 'react';
 
 function App() {
+  const onDelete = (todo) => {
+    console.log("Ondelete of ", todo);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }))
+  }
+
+  const [todos, setTodos] = useState([
+    {
+      sno: 1,
+      title: "It is title 1",
+      desc: "It is description 1"
+    },
+    {
+      sno: 2,
+      title: "It is title 2",
+      desc: "It is description 2"
+    },
+    {
+      sno: 3,
+      title: "It is title 3",
+      desc: "It is description 3"
+    }
+  ]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="My Todos List" />
+      <AddTodo/>
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Footer = () => {
+    let footerStyle = {
+        position: "absolute",
+        width: "100%",
+        top: "90vh"
+    }
+    return (
+        <footer className="bg-dark text-light py-3" style={footerStyle}>
+            <p className="text-center">Copyright &copy; MyTodoList.com </p>
+        </footer>
+    )
+}
